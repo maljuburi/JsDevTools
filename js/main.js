c7aca110886be5a76c2990d2a98c95356a51a353
Muhammad <maljuburi89@gmail.com>
2017-09-19 00:15:03 -0400

    First Script

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var dropdown = document.getElementById("dropdown");
+var subMenu = document.getElementById("subMenu");
+
+dropdown.addEventListener('click', showSubMenu);
+
+function showSubMenu(){
+  if (subMenu.style.display == "block"){
+    subMenu.style.display="none";
+  }else{
+    subMenu.style.display="block";
+  }
+
+}
