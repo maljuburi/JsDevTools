d3b21b3dfae48477e9f1a7045611fc93797f1bf6
Muhammad <maljuburi89@gmail.com>
2017-09-27 02:48:31 -0400

    Module 3

--- js/main.js
+++ js/main.js
@@ -11,3 +11,50 @@ function showSubMenu(){
   }
 
 }
+
+
+
+
+//=================
+// Image Slider
+// ===================
+
+
+//initializing index
+var index = 1;
+
+// going to the next slide
+function plusSlide(n){
+  // update showSlide
+  showSlide(index += n);
+}
+
+//calling showSlide
+showSlide(index);
+
+// Showing next/prev slide
+function showSlide(n){
+
+  // graping all slides
+  var slides = document.getElementsByClassName("slide");
+
+  // after last slide view first slide
+  if (n > slides.length) {
+    index = 1;
+  }
+
+  // view last slide if going back from first slide.
+  if (n < 1) {
+    index = slides.length;
+  }
+
+  // looping through the slides and hiding all
+  for(var i=0; i<slides.length; i++){
+    slides[i].style.display="none";
+  }
+
+  // showing current slide
+  slides[index-1].style.display ="block";
+
+
+}
